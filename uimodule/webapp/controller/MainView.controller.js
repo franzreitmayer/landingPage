f15b4fbd919894b9688bb4293b29b5a171ca7e33
Franz Reitmayer <franz.reitmayer@reitmayer.eu>
2021-11-20 13:47:58 +0100

    feature: first page and social media links

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -1,5 +1,5 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "eu/reitmayer/ui5/landingPage/controller/BaseController"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
@@ -10,6 +10,25 @@ sap.ui.define([
         return Controller.extend("eu.reitmayer.ui5.landingPage.controller.MainView", {
             onInit: function () {
 
+            },
+
+            onSocialLink: function(eventSource) {
+                var sourceId = eventSource.getSource().data("id");
+                console.log(sourceId);
+                var resourceBundle = this.getResourceBundle();
+                switch (sourceId) {
+                    case "github": {
+                        // alert("clicked on github");
+                        //window.open("https://github.com/franzreitmayer", '_blank').focus();
+                        
+                        
+                        let url = resourceBundle.getText("profiles.github.url");
+                        let newTab = window.open();
+                        newTab.location.href = url;
+                        break;
+                    }
+                    
+                }
             }
         });
     });
